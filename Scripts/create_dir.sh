9255d4ffc1ae73ef8990a403824a289f2370cd70
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 18:59:06 -0600

    modified appspecfile

--- Scripts/create_dir.sh
+++ Scripts/create_dir.sh
@@ -3,6 +3,3 @@
 # Create directory and cd into directory
 mkdir /home/ec2-user/modern_stays
 cd /home/ec2-user/modern_stays
-
-#install packages
-npm install
---
+++ Scripts/install_nvm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.nvm/nvm.sh
