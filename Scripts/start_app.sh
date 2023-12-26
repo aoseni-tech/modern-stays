26dedad3ff25600af20b6e37a032e97a194efe64
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 22:08:25 -0600

    modified appspecfile

--- Scripts/start_app.sh
+++ Scripts/start_app.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 export AWS_MODERNSTAY_CONFIG="$(aws secretsmanager get-secret-value --secret-id Modern_Stays_Variables)"
 
 npm run start
