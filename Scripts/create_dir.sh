44dfb11abecf01f6ffb13e9c571b20483c1e5161
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 19:08:24 -0600

    modified appspecfile

--- Scripts/create_dir.sh
+++ Scripts/create_dir.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Create directory and cd into directory
-mkdir /home/ec2-user/modern_stays
-cd /home/ec2-user/modern_stays
+mkdir /home/ec2-user/modern_stays && cd /home/ec2-user/modern_stays
