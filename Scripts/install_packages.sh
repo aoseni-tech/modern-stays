5fc76bd82c485f4fcb0b1933cf52532844ce1e48
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 21:09:25 -0600

    modified appspecfile

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /home/ec2-user/.bash_profile
+
 cd /home/ec2-user/modern_stays
 
 #install packages
--- Scripts/start_app.sh
+++ Scripts/start_app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /home/ec2-user/.bash_profile
+
 cd /home/ec2-user/modern_stays
 
 /home/ec2-user/.nvm/versions/node/v20.10.0/bin/npm run start
