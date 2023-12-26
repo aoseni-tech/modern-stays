f0123fada3eb3dfa6cf8787eff709b6be47b0cd2
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 23:39:43 -0600

    modified appspecfile

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -5,5 +5,8 @@ cd /home/ec2-user/
 #install packages
 npm install
 
+#install pm2
+npm install pm2 -g
+
 #build files
 npm run build
--- Scripts/start_app.sh
+++ Scripts/start_app.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/
 
 export AWS_MODERNSTAY_CONFIG="$(aws secretsmanager get-secret-value --secret-id Modern_Stays_Variables)"
 
-npm run start
+pm2 start dist/index.js
