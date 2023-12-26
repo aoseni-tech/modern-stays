f5f51eee14787f63b1cd4760d5ee534d4256de48
goldenacetech <goldenacetech@hotmail.com>
2023-12-25 21:24:50 -0600

    modified appspecfile

--- Scripts/install_packages.sh
+++ Scripts/install_packages.sh
@@ -3,7 +3,8 @@
 sh /home/ec2-user/.nvm/nvm.sh
 source /home/ec2-user/.bash_profile
 
-cd /home/ec2-user/modern_stays
-
 #install packages
 /home/ec2-user/.nvm/versions/node/v20.10.0/bin/npm install
+
+#build files
+/home/ec2-user/.nvm/versions/node/v20.10.0/bin/npm run build
--- Scripts/start_app.sh
+++ Scripts/start_app.sh
@@ -3,6 +3,6 @@
 sh /home/ec2-user/.nvm/nvm.sh
 source /home/ec2-user/.bash_profile
 
-cd /home/ec2-user/modern_stays
+export AWS_MODERNSTAY_CONFIG="$(aws secretsmanager get-secret-value --secret-id Modern_Stays_Variables)"
 
 /home/ec2-user/.nvm/versions/node/v20.10.0/bin/npm run start
