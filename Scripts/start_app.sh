d1fba711b7cf7cf39f45e4aa51aac524191d47f2
goldenacetech <goldenacetech@hotmail.com>
2023-12-28 07:51:42 -0600

    added stop script and updated appspecfile

--- Scripts/start_app.sh
+++ Scripts/start_app.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/
 
 export AWS_MODERNSTAY_CONFIG="$(aws secretsmanager get-secret-value --secret-id Modern_Stays_Variables)"
 
-# pm2 start dist/index.js
+pm2 start dist/index.js
---
+++ Scripts/stop_app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/ec2-user/
+
+pm2 stop dist/index.js
